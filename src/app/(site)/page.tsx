6b46a9e3dd44d0ad5347ce840f15120fde8764fc
Jyriz <discordketukangan123@gmail.com>
2024-11-18 10:51:08 +0700

    refactor: test

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar";
 const Home = () => {
   return (
     <>
+    {/* test */}
       <Navbar />
       <div className="px-12 md:px-24 py-3 lg:px-52 transition-all">
         <p>ea</p>
